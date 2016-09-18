8642f4514f2ab3684a5d2d7d116f5d0d1b11c086
Valkyres <114sol@naver.com>
2016-09-18 17:49:15 +0900

    모바증실습과제용
    모바증실습과제용

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Transform transformA;
+    public Transform transformB;
+    // Use this for initialization
+    void Start() {
+
+   
+}
+
+  
+    // Update is called once per frame
+    void Update () {
+        Debug.Log((transformB.position - transformA.position).magnitude);
+    }
+}
